f04b1a9da7aaff436e238b0c20b8c708b89e6ee5
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-04-22 01:13:49 +0330

    feat(demo/form-builder): listener demo

--- apps/demo/src/form-builder/script.ts
+++ apps/demo/src/form-builder/script.ts
@@ -1,30 +1,67 @@
 import '@gecut/form-builder';
 
 const form = document.createElement('form-builder');
+const formBox = document.querySelector<HTMLFormElement>('.form');
+const result = document.querySelector<HTMLPreElement>('.result');
+
+const listener = () => {
+  if (result != null) {
+    result.innerHTML = JSON.stringify(form.data, null, 4);
+  }
+};
 
 form.data = {
   components: [
-    {
-      name: 'text',
-      label: 'text',
-      type: 'text',
-      ui: 'filled',
-    },
     [
       {
-        name: 'text',
-        label: 'text',
+        name: 'first-name',
+        label: 'First Name',
         type: 'text',
         ui: 'filled',
       },
       {
-        name: 'text',
-        label: 'text',
+        name: 'last-name',
+        label: 'Last Name',
         type: 'text',
         ui: 'filled',
       },
     ],
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      ui: 'filled',
+    },
+    {
+      name: 'tel',
+      label: 'Phone Number',
+      type: 'tel',
+      ui: 'filled',
+    },
+    [
+      {
+        name: 'password',
+        label: 'Password',
+        type: 'password',
+        ui: 'filled',
+      },
+      {
+        name: 'confirm-password',
+        label: 'Confirm Password',
+        type: 'password',
+        ui: 'filled',
+      },
+    ],
+    {
+      type: 'submit',
+      label: 'Submit',
+      ui: 'filled',
+      align: 'end',
+    },
   ],
+  listener,
 };
 
-document.body.appendChild(form);
+formBox?.appendChild(form);
+
+listener();
