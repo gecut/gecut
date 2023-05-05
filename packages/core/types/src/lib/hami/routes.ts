4a23130813eccbd18015001d2612775b8784cb31
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-05 20:32:37 +0330

    feat(core/types/hami): change routes

--- packages/core/types/src/lib/hami/routes.ts
+++ packages/core/types/src/lib/hami/routes.ts
@@ -1,8 +1,10 @@
 import type {CustomerModel} from './customer.js';
 import type {UserModel} from './user.js';
+import type {Notification} from './notification.js';
 import type {AlwatrDocumentStorage} from '@alwatr/type/storage.js';
 
 export interface Routes {
-  '/customer-list/': Promise<AlwatrDocumentStorage<CustomerModel>>;
-  '/user-list/': Promise<AlwatrDocumentStorage<UserModel>>;
+  'customer-storage': Promise<AlwatrDocumentStorage<CustomerModel>>;
+  'user-storage': Promise<AlwatrDocumentStorage<UserModel>>;
+  'notification-storage': Promise<AlwatrDocumentStorage<Notification>>;
 }
