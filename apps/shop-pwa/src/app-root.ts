14ac83e6ca1a4208db95afb1f1d44efa071916d5
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-05 21:01:42 +0330

    feat(apps/shop-pwa): remove project

--- apps/shop-pwa/src/app-root.ts
+++
@@ -1,3 +0,0 @@
-import './config';
-import './providers/get-products-list';
-import './ui/components/app/app';
--- apps/shop-pwa/src/utils/query-params.ts
+++
@@ -1,5 +0,0 @@
-export type QueryParams = Record<string, string>;
-
-export const dataToQuery = (data: QueryParams) => {
-  return new URLSearchParams(data).toString();
-};
