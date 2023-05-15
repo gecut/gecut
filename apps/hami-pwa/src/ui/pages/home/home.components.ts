e4de98e5205cad56e2306a7d4ba7a3890b09f130
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-15 22:48:25 +0330

    feat(apps/hami-pwa): product price card

---
+++ apps/hami-pwa/src/ui/pages/home/home.components.ts
@@ -0,0 +1,174 @@
+import { renderListItem, renderTextField } from '@gecut/components';
+import { signalElement } from '@gecut/mixins';
+import { html, nothing } from 'lit';
+import { state } from 'lit/decorators/state.js';
+import { repeat } from 'lit/directives/repeat.js';
+import IconDoneRounded from 'virtual:icons/material-symbols/done-rounded';
+import IconErrorOutlineRounded from 'virtual:icons/material-symbols/error-outline-rounded';
+import IconStarOutlineRounded from 'virtual:icons/material-symbols/star-outline-rounded';
+import IconWarningOutlineRounded from 'virtual:icons/material-symbols/warning-outline-rounded';
+
+import i18n from '../../i18n';
+
+import type { RenderResult } from '@gecut/types';
+import type { Notification } from '@gecut/types/hami/notification';
+import type { ProductPrice } from '@gecut/types/hami/product-price';
+
+export class HomeComponents extends signalElement {
+  @state()
+  protected notifications: Notification[] = [];
+
+  @state()
+  protected productsPrice: ProductPrice[] = [];
+
+  @state()
+  protected productsPriceSearchQuery = '';
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    this.addSignalListener('notification-storage', (value) => {
+      this.notifications = Object.values(value.data);
+    });
+    this.addSignalListener('product-price-storage', (value) => {
+      this.productsPrice = Object.values(value.data);
+    });
+  }
+
+  static renderNotificationItem(notification: Notification): HTMLElement {
+    let icon = '';
+
+    const messageWords = notification.message.split(' ');
+    const messageLength = messageWords.length;
+    const messageHeadlineBreakIndex = Math.round(messageLength / 3);
+    const messageHeadline = messageWords
+      .slice(0, messageHeadlineBreakIndex)
+      .join(' ');
+    const messageSupportingText = messageWords
+      .slice(messageHeadlineBreakIndex, messageLength)
+      .join(' ');
+
+    switch (notification.status) {
+    case 'danger':
+      icon = IconErrorOutlineRounded;
+      break;
+    case 'warning':
+      icon = IconWarningOutlineRounded;
+      break;
+    case 'normal':
+      icon = IconStarOutlineRounded;
+      break;
+    case 'success':
+      icon = IconDoneRounded;
+      break;
+    }
+
+    return renderListItem({
+      component: 'list-item',
+      type: 'list-item',
+      headline: messageHeadline,
+      supportingText: messageSupportingText,
+      multiLineSupportingText: true,
+      classes: ['notification-item'],
+      slotList: [
+        {
+          component: 'icon',
+          type: 'svg',
+          slot: 'start',
+          SVG: icon,
+          classes: [`icon-${notification.status}`],
+        },
+      ],
+    });
+  }
+  static renderProductPriceItem(productPrice: ProductPrice): HTMLElement {
+    return renderListItem({
+      component: 'list-item',
+      type: 'list-item',
+      headline: productPrice.name,
+      supportingText: productPrice.normalPrice.toLocaleString(),
+      trailingSupportingText: productPrice.minPrice.toLocaleString(),
+      classes: ['product-price-item'],
+    });
+  }
+
+  protected renderNotificationCard(): RenderResult {
+    if (this.notifications.length === 0) return nothing;
+
+    return html`
+      <div class="card-box">
+        <h3 class="title">
+          ${i18n.message('home_page_notification_box_title')}
+        </h3>
+
+        <div class="card">
+          <div class="card-scroll">
+            <md-list>
+              ${repeat(
+    this.notifications,
+    (notification) => notification.id,
+    HomeComponents.renderNotificationItem
+  )}
+            </md-list>
+          </div>
+
+          <md-elevation></md-elevation>
+        </div>
+      </div>
+    `;
+  }
+
+  protected renderProductPriceSearchBox(): HTMLElement {
+    return renderTextField({
+      component: 'text-field',
+      type: 'outlined',
+
+      inputType: 'search',
+      name: 'productPriceSearch',
+      placeholder: 'product-price-search',
+
+      customConfig: (target) => {
+        target.addEventListener('input', () => {
+          this.productsPriceSearchQuery = target.value;
+        });
+
+        return target;
+      },
+    });
+  }
+  protected renderProductPriceCard(): RenderResult {
+    if (this.productsPrice.length === 0) return nothing;
+
+    let productsPrice = this.productsPrice;
+
+    if (this.productsPriceSearchQuery.trim() !== '') {
+      productsPrice = this.productsPrice.filter((productPrice) => {
+        return productPrice.name.indexOf(this.productsPriceSearchQuery) != -1;
+      });
+    }
+
+    return html`
+      <div class="card-box">
+        <h3 class="title">
+          ${i18n.message('home_page_product_price_box_title')}
+        </h3>
+
+        <div class="search-box">${this.renderProductPriceSearchBox()}</div>
+
+        <div class="card">
+          <div class="card-scroll">
+            <md-list>
+              ${repeat(
+    productsPrice,
+    (productPrice) => productPrice.id,
+    HomeComponents.renderProductPriceItem
+  )}
+            </md-list>
+          </div>
+
+          <md-elevation></md-elevation>
+        </div>
+      </div>
+    `;
+  }
+}
--- apps/hami-pwa/src/ui/pages/home/home.page.ts
+++ apps/hami-pwa/src/ui/pages/home/home.page.ts
@@ -1,28 +1,16 @@
-import { html, nothing, unsafeCSS } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
-import { signalElement } from '@gecut/mixins';
-import { map } from 'lit/directives/map.js';
-import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import { dispatch, request } from '@gecut/signal';
-import IconDoneRounded from 'virtual:icons/material-symbols/done-rounded';
-import IconErrorOutlineRounded from 'virtual:icons/material-symbols/error-outline-rounded';
-import IconWarningOutlineRounded from 'virtual:icons/material-symbols/warning-outline-rounded';
-import IconStarOutlineRounded from 'virtual:icons/material-symbols/star-outline-rounded';
-
-import '@material/web/list/list';
-import '@material/web/list/list-item';
-import '@material/web/icon/icon';
+import { html, unsafeCSS } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { cache } from 'lit/directives/cache.js';
 
 import { requireSignIn } from '../../../controllers/require-sign-in';
+import { urlForName } from '../../router';
 import elementStyle from '../../stylesheets/element.scss?inline';
 import pageStyle from '../../stylesheets/page.scss?inline';
-import i18n from '../../i18n';
-import { urlForName } from '../../router';
 
+import { HomeComponents } from './home.components';
 import styles from './home.page.scss?inline';
 
-import type { MdListItem } from '@material/web/list/list-item';
-import type { Notification } from '@gecut/types/hami/notification';
 import type { RenderResult } from '@gecut/types';
 
 declare global {
@@ -32,15 +20,12 @@ declare global {
 }
 
 @customElement('page-home')
-export class PageHome extends signalElement {
+export class PageHome extends HomeComponents {
   static override styles = [
     unsafeCSS(styles),
     unsafeCSS(elementStyle),
     unsafeCSS(pageStyle),
   ];
-
-  @state()
-  private notifications: Notification[] = [];
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -52,11 +37,8 @@ export class PageHome extends signalElement {
 
     this.addEventListener('scroll', this.topAppBarChangeMode);
 
-    this.addSignalListener('notification-storage', (value) => {
-      this.notifications = Object.values(value.data);
-    });
-
     request('notification-storage', {});
+    request('product-price-storage', {});
   }
   override disconnectedCallback(): void {
     super.disconnectedCallback();
@@ -67,81 +49,8 @@ export class PageHome extends signalElement {
   override render(): RenderResult {
     super.render();
 
-    return PageHome.renderNotificationCard(this.notifications);
-  }
-
-  static openDetailNotificationItem(event: Event): void {
-    const target = event.target as MdListItem;
-
-    target.multiLineSupportingText = true;
-  }
-
-  static closeDetailNotificationItem(event: Event): void {
-    const target = event.target as MdListItem;
-
-    target.multiLineSupportingText = false;
-  }
-
-  static renderNotificationItem(notification: Notification): RenderResult {
-    let icon = '';
-
-    const messageWords = notification.message.split(' ');
-    const messageLength = messageWords.length;
-    const messageHeadlineBreakIndex = Math.round(messageLength / 3);
-    const messageHeadline = messageWords
-        .slice(0, messageHeadlineBreakIndex)
-        .join(' ');
-    const messageSupportingText = messageWords
-        .slice(messageHeadlineBreakIndex, messageLength)
-        .join(' ');
-
-    switch (notification.status) {
-      case 'danger':
-        icon = IconErrorOutlineRounded;
-        break;
-      case 'warning':
-        icon = IconWarningOutlineRounded;
-        break;
-      case 'normal':
-        icon = IconStarOutlineRounded;
-        break;
-      case 'success':
-        icon = IconDoneRounded;
-        break;
-    }
-
     return html`
-      <md-list-item
-        class="notification-item"
-        headline=${messageHeadline}
-        supportingText=${messageSupportingText}
-        @focus=${PageHome.openDetailNotificationItem}
-        @blur=${PageHome.closeDetailNotificationItem}
-      >
-        <md-icon slot="start" class="icon-${notification.status}">
-          ${unsafeSVG(String(icon))}
-        </md-icon>
-      </md-list-item>
-    `;
-  }
-
-  static renderNotificationCard(notifications: Notification[]): RenderResult {
-    if (notifications.length === 0) return nothing;
-
-    return html`
-      <div class="card-box">
-        <h3 class="title">${i18n.message('notification_box_title')}</h3>
-
-        <div class="card">
-          <div class="card-scroll">
-            <md-list>
-              ${map(notifications, PageHome.renderNotificationItem)}
-            </md-list>
-          </div>
-
-          <md-elevation></md-elevation>
-        </div>
-      </div>
+      ${cache(this.renderNotificationCard())} ${this.renderProductPriceCard()}
     `;
   }
 
