5c872d0e356a7e8d2c0cbf09151eebe0a9d89fa4
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-17 03:11:16 +0330

    refactor(apps/hami-pwa): css layout

--- apps/hami-pwa/src/app-root.ts
+++ apps/hami-pwa/src/app-root.ts
@@ -1,5 +1,6 @@
-import '@gecut/common/styles/mobile-only.css';
 import '@gecut/common/styles/pwa.css';
+// eslint-disable-next-line import/order
+import '@gecut/common/styles/mobile-only.css';
 import '@gecut/common/styles/theme/palettes/cadmium-green.css';
 import '@gecut/common/styles/tokens.css';
 import 'unfonts.css';
