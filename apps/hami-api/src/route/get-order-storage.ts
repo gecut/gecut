e20de1e0fdccb2a6febfd95630d42085afcffac6
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-24 00:12:13 +0330

    feat(apps/hami-api): get order storage

--- apps/hami-api/src/route/get-order-storage.ts
+++ apps/hami-api/src/route/get-order-storage.ts
@@ -5,24 +5,54 @@ import { requireSignedIn } from '../util/require-signed-in';
 
 import type { Projects } from '@gecut/types';
 
-nanoServer.route('GET', '/order-storage/', async (connection) => {
-  logger.logMethod('get-order-storage');
+nanoServer.route(
+  'GET',
+  '/order-storage/',
+  async (connection): Promise<Projects.Hami.Routes['order-storage']> => {
+    logger.logMethod('get-order-storage');
 
-  const user = await requireSignedIn(connection);
-  const orderStorage = await storageClient.getStorage<Projects.Hami.Order>(
-    config.orderStorage
-  );
+    const user = await requireSignedIn(connection);
+    const orderStorage =
+      await storageClient.getStorage<Projects.Hami.OrderModel>(
+        config.orderStorage
+      );
 
-  if (user.role === 'admin') {
-    return orderStorage;
+    for await (const orderId of Object.keys(orderStorage)) {
+      const order = orderStorage.data[orderId];
+      const creator = await storageClient.get<Projects.Hami.User>(
+        order.creatorId,
+        config.userStorage
+      );
+      const customer = await storageClient.get<Projects.Hami.Customer>(
+        order.customerId,
+        config.customerStorage
+      );
+      const customerProject =
+        await storageClient.get<Projects.Hami.CustomerProject>(
+          order.customerProjectId,
+          config.customerProjectStoragePrefix + order.customerId
+        );
+
+      orderStorage.data[orderId] = {
+        creator,
+        customer,
+        customerProject,
+
+        ...order,
+      };
+    }
+
+    if (user.role === 'admin') {
+      return orderStorage;
+    }
+
+    return {
+      ...orderStorage,
+      data: Object.fromEntries(
+        Object.entries(orderStorage.data).filter(
+          (order) => order[1].creatorId === user.id
+        )
+      ),
+    };
   }
-
-  return {
-    ...orderStorage,
-    data: Object.fromEntries(
-      Object.entries(orderStorage.data).filter(
-        (order) => order[1].creatorId === user.id
-      )
-    ),
-  };
-});
+);
