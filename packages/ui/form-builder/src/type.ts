d14d1d7c882bd1cab836927c02e6959026f4f24a
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-27 00:44:54 +0330

    style(ui/form-builder): prettier

--- packages/ui/form-builder/src/type.ts
+++ packages/ui/form-builder/src/type.ts
@@ -2,7 +2,9 @@ import type { Rules } from '@gecut/form-validator';
 import type { SingleOrArray } from '@gecut/types';
 import type { M3 } from '@gecut/ui-kit';
 
-export type FormTextFieldContent = M3.Types.TextFieldContent & { validator?: Rules };
+export type FormTextFieldContent = M3.Types.TextFieldContent & {
+  validator?: Rules;
+};
 export type FormButtonContent = M3.Types.ButtonContent & {
   disabled?: boolean | 'validate' | ((form: Form, slide: FormSlide) => boolean);
   action?: 'next_slide' | 'previous_slide' | 'form_submit';
