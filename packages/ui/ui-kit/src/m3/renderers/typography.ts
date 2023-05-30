73ce51e102b3934b97ff29b7addc536c8cf4f44f
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-05-31 00:47:06 +0330

    feat(ui/ui-kit/typography): new renderer & type

---
+++ packages/ui/ui-kit/src/m3/renderers/typography.ts
@@ -0,0 +1,34 @@
+import '@material/web/button/elevated-button';
+import '@material/web/button/filled-button';
+import '@material/web/button/outlined-button';
+import '@material/web/button/text-button';
+import '@material/web/button/tonal-button';
+
+import { typographyStyles } from '../types/typography';
+
+import { createElementByContent } from './base/base-renderer';
+
+import type {
+  TypoGraphyContent,
+  TypoGraphyRendererReturn,
+  TypoGraphyStylesProperties} from '../types/typography';
+
+export function renderTypoGraphy(
+  content: TypoGraphyContent
+): TypoGraphyRendererReturn {
+  const typography = createElementByContent(content.type, content, []);
+
+  if (content.style != null) {
+    typography.style.margin = '0';
+
+    const selectedStyle = typographyStyles[content.style];
+
+    for (const property of Object.keys(selectedStyle)) {
+      const _prop = property as TypoGraphyStylesProperties;
+
+      typography.style[_prop] = selectedStyle[_prop];
+    }
+  }
+
+  return typography;
+}
