5fff7d82e08798dfefb7393e342d7f4a6123856b
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-06-09 23:12:41 +0330

    feat(apps/hami-pwa/icons): define all icons

---
+++ apps/hami-pwa/src/ui/icons/filled-rounded.ts
@@ -0,0 +1,27 @@
+import add from 'virtual:icons/material-symbols/add-rounded';
+import alternateEmail from 'virtual:icons/material-symbols/alternate-email-rounded';
+import attachMoney from 'virtual:icons/material-symbols/attach-money-rounded';
+import awardStar from 'virtual:icons/material-symbols/award-star-rounded';
+import done from 'virtual:icons/material-symbols/done-rounded';
+import grading from 'virtual:icons/material-symbols/grading-rounded';
+import group from 'virtual:icons/material-symbols/group-rounded';
+import home from 'virtual:icons/material-symbols/home-rounded';
+import login from 'virtual:icons/material-symbols/login-rounded';
+import menu from 'virtual:icons/material-symbols/menu-rounded';
+import person from 'virtual:icons/material-symbols/person-rounded';
+import search from 'virtual:icons/material-symbols/search-rounded';
+
+export const filledRounded = {
+  add,
+  alternateEmail,
+  attachMoney,
+  awardStar,
+  done,
+  grading,
+  group,
+  home,
+  login,
+  menu,
+  person,
+  search,
+};
