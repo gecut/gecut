a97161db4e32dfa64e0b316134d1ed23ddacaef9
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-06-18 20:45:44 +0330

    feat(apps/hami-pwa/content): customer dialog

---
+++ apps/hami-pwa/src/content/dialogs/customer-dialog.ts
@@ -0,0 +1,38 @@
+import i18n from '#hami/ui/i18n';
+
+import { customerCard } from '../cards/customer-card';
+import { customerProjectListCard } from '../cards/customer-projects-list-card';
+import { orderCard } from '../cards/order-card';
+import { headingPageTypography } from '../typographies/heading-page-typography';
+
+import type { Projects } from '@gecut/types';
+import type { M3 } from '@gecut/ui-kit';
+
+export function customerDialog(
+  customer: Projects.Hami.CustomerModel
+): M3.Types.DialogContent {
+  return {
+    component: 'dialog',
+    type: 'dialog',
+    fullscreen: true,
+    slotList: [
+      headingPageTypography(i18n.message('content_customer_dialog'), {
+        slot: 'headline',
+      }),
+      customerCard(customer),
+      customerProjectListCard(customer.projectList),
+      ...customer.orderList.slice(0, 15).map((order) => orderCard(order)),
+      {
+        component: 'button',
+        type: 'text',
+        label: i18n.message('customers_information_box_dialog_close_label'),
+        slot: 'footer',
+        customConfig: (target) => {
+          target.setAttribute('dialogAction', 'close');
+
+          return target;
+        },
+      },
+    ],
+  };
+}
