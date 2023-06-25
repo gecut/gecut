66a90688ac922329f88fab6af1601354fdf0c35b
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-06-25 23:17:24 +0330

    feat(packages/utilities): new package

---
+++ packages/utilities/src/polyfill.ts
@@ -0,0 +1,29 @@
+interface IndexableWindow {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+}
+
+export const win = globalThis as IndexableWindow;
+
+const requestAnimationFrameFallback = (
+  callback: FrameRequestCallback
+): ReturnType<typeof setTimeout> =>
+  setTimeout(() => callback(Date.now()), 1000 / 60);
+
+export const gecutAnimationFrame: typeof globalThis.requestAnimationFrame =
+  win['requestAnimationFrame'] ||
+  win['webkitRequestAnimationFrame'] ||
+  win['mozRequestAnimationFrame'] ||
+  requestAnimationFrameFallback;
+
+const requestIdleCallbackFallback = (
+  callback: () => void,
+  options?: IdleRequestOptions
+): ReturnType<typeof setTimeout> =>
+  setTimeout(callback, options?.timeout ?? 2000);
+
+export const gecutIdleCallback: typeof globalThis.requestIdleCallback =
+  win['requestIdleCallback'] ||
+  win['webkitRequestIdleCallback'] ||
+  win['mozRequestIdleCallback'] ||
+  requestIdleCallbackFallback;
