38eb5a0b79aaf4bf7d1b256ce730aab10cae29ca
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-07-12 00:43:40 +0330

    feat(apps/persianpart-pwa): icons

--- apps/persianpart-pwa/src/ui/icons/filled-rounded.ts
+++ apps/persianpart-pwa/src/ui/icons/filled-rounded.ts
@@ -1,3 +1,4 @@
 import logout from 'virtual:icons/material-symbols/logout-rounded';
+import search from 'virtual:icons/material-symbols/search-rounded';
 
-export const filledRounded = { logout };
+export const filledRounded = { logout, search };
